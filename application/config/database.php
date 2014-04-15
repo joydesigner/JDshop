f6b64bf179ba85cf4577665b25b73567632de5aa
Lixing <zlxjackie@hotmail.com>
2014-04-15 22:48:53 +1000

    Update database sql

--- application/config/database.php
+++ application/config/database.php
@@ -49,11 +49,11 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'jdshop';
+$db['default']['password'] = 'jdshop';
+$db['default']['database'] = 'jdshop';
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbprefix'] = 'jd_';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
